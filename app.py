5beadff1ba3e43f43f3734ca5a33d1cd60c777f4
edmilsoncosme <edmilsoncosme@unb.br>
2024-07-04 16:49:34 -0300

    Update - Adicionando o código

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from streamlit_ace import st_ace, THEMES, LANGUAGES
 
+st.title("Nosso Editor de Texto")
+
+c1 , c2 = st.columns([3, 1])
+
+with c1:
+    content = st_ace(
+        theme=c2.selectbox('Tema', options=THEMES),
+        language=c2.selectbox('Linguagem', options=LANGUAGES, index=121),
+        font_size=c2.slider('Tam. Fonte', 25),
+        min_lines=50
+    )
+
+    content
